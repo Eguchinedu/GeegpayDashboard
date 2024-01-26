988cff548badaffb2fcdf86ff1f6e72a0522a570
Egu Chinedu <eguchinedu18@gmail.com>
2024-01-26 23:41:28 +0100

    redeployed changes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,13 +6,16 @@ import { Component, Input, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent  {
+export class HeaderComponent {
   @Input() header = '';
 
- currentDate: string | null;
+  currentDate: string | null;
+  changeBtn: boolean = false;
 
   constructor(private datePipe: DatePipe) {
     this.currentDate = this.datePipe.transform(new Date(), 'MMMM d, yyyy');
-    
+  }
+  changeBtnState() {
+    this.changeBtn = !this.changeBtn;
   }
 }
--- src/app/pages/dashboard/card-one/card-one.component.ts
+++ src/app/pages/dashboard/card-one/card-one.component.ts
@@ -3,6 +3,81 @@ import { Chart, registerables } from 'chart.js';
 import { ServiceService } from 'src/app/service/service.service';
 Chart.register(...registerables);
 
+const chartData = [
+  {
+    month: 'Jan',
+    amount: 6000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Feb',
+    amount: 20000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Mar',
+    amount: 3000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Apr',
+    amount: 25000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'May',
+    amount: 8000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Jun',
+    amount: 45000,
+    color: 'rgba(52, 202, 165, 1), rgba(52, 202, 165, 0)',
+    borderColor: 'rgba(52, 202, 165, 1), rgba(52, 202, 165, 0)',
+  },
+  {
+    month: 'Jul',
+    amount: 8000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Aug',
+    amount: 22000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Sep',
+    amount: 35000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Oct',
+    amount: 4000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Nov',
+    amount: 30000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+  {
+    month: 'Dec',
+    amount: 25000,
+    color: 'rgba(52, 202, 165, 0.1)',
+    borderColor: 'rgba(52, 202, 165, 0.1)',
+  },
+];
+
 @Component({
   selector: 'app-card-one',
   templateUrl: './card-one.component.html',
@@ -17,18 +92,27 @@ export class CardOneComponent implements OnInit {
 
   constructor(private _barService: ServiceService) {}
   ngOnInit(): void {
-    this._barService.showData().subscribe((data) => {
-      this.data = data;
 
-      if (this.data != null) {
-        for (let i = 0; i < this.data.length; i++) {
-          this.datamonth.push(this.data[i].month);
-          this.dataamount.push(this.data[i].amount);
-          this.datacolor.push(this.data[i].color);
-          this.databorder.push(this.data[i].borderColor);
-        }
-      }
-    });
+     for (let i = 0; i < chartData.length; i++) {
+       this.datamonth.push(chartData[i].month);
+       this.dataamount.push(chartData[i].amount);
+       this.datacolor.push(chartData[i].color);
+       this.databorder.push(chartData[i].borderColor);
+     }
+    // this._barService.showData().subscribe((data) => {
+    //   this.data = data;
+    //   console.log(data);
+      
+
+    //   if (this.data != null) {
+    //     for (let i = 0; i < this.data.length; i++) {
+    //       this.datamonth.push(this.data[i].month);
+    //       this.dataamount.push(this.data[i].amount);
+    //       this.datacolor.push(this.data[i].color);
+    //       this.databorder.push(this.data[i].borderColor);
+    //     }
+    //   }
+    // });
     this.showChartData(
       this.datamonth,
       this.dataamount,
@@ -54,7 +138,6 @@ export class CardOneComponent implements OnInit {
         ],
       },
       options: {
-        responsive: true,
         plugins: {
            legend: {
                 display: false,
