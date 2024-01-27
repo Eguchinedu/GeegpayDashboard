8d1377fe8a2129c084db681ce5647aada973f61b
Egu Chinedu <eguchinedu18@gmail.com>
2024-01-27 04:30:09 +0100

    added theme to general project

--- src/app/components/coming-soon/coming-soon.component.ts
+++ src/app/components/coming-soon/coming-soon.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from 'src/app/service/dark-mode.service';
 
 @Component({
   selector: 'app-coming-soon',
   templateUrl: './coming-soon.component.html',
-  styleUrls: ['./coming-soon.component.css']
+  styleUrls: ['./coming-soon.component.css'],
 })
 export class ComingSoonComponent {
-
+  constructor(public darkModeService: DarkModeService) {}
 }
