6f7f825a999a4fa6ff9c6cca9690530651ee9205
Egu Chinedu <eguchinedu18@gmail.com>
2024-01-27 04:14:11 +0100

    added theme for card three

--- src/app/pages/dashboard/card-two/card-two.component.ts
+++ src/app/pages/dashboard/card-two/card-two.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { DarkModeService } from 'src/app/service/dark-mode.service';
 
 @Component({
   selector: 'app-card-two',
   templateUrl: './card-two.component.html',
-  styleUrls: ['./card-two.component.css']
+  styleUrls: ['./card-two.component.css'],
 })
 export class CardTwoComponent {
-
+  constructor(public darkModeService: DarkModeService) {}
 }
